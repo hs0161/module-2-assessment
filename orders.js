5bab2a6758db73a3222a82993e73bef528077254
Hannah Song <hannahsong89@gmail.com>
2022-12-05 22:23:37 -0600

    final commit

--- orders.js
+++ orders.js
@@ -28,8 +28,18 @@
     Where CUSTOMERID and STATUS reference the values
     stored on the object.
 */
-
-//CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = "queued";
+    }
+    updateStatus(newStatus) {
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerId} is now ${this.status}`)
+    }
+}
 
 
 
@@ -44,7 +54,7 @@
     customer: 575
 */
 
-//CODE HERE
+const firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 PM','575')
 
 
 /*
@@ -53,4 +63,5 @@
     'cooking'
 */
 
-//CODE HERE
+
+firstTicket.updateStatus("cooking")
